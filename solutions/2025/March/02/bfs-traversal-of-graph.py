18edf3fd9ca5de177cfc1f7d7488027833776dd5
kartik-mundkar <141703155+kartik-mundkar@users.noreply.github.com>
2025-03-02 15:51:46 +0530

    Updated: solutions/2025/March/02/bfs-traversal-of-graph.py

---
+++ solutions/2025/March/02/bfs-traversal-of-graph.py
@@ -0,0 +1,6 @@
+#User function Template for python3
+class Solution:
+    # Function to return Breadth First Traversal of given graph.
+    def bfsOfGraph(self, adj: List[List[int]]) -> List[int]:
+        # code here
+...# } Driver Code Ends
