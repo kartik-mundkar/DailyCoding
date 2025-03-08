aac2847035ea68324faa83373434c14bbff530c8
Kartik Mundkar <141703155+kartik-mundkar@users.noreply.github.com>
2025-03-09 00:06:56 +0530

    Updated: solutions/2025/March/09/closest-prime-numbers-in-range.java

--- solutions/2025/March/09/closest-prime-numbers-in-range.java
+++ solutions/2025/March/09/closest-prime-numbers-in-range.java
@@ -1,12 +1,12 @@
                     allPrimes[j]=false;
+        for(int i=2;i<=right;i++){
+            if (allPrimes[i]){
                 for(int j=2*i;j<=right;j=j+i){
             allPrimes[i]=true;
         }
-        for(int i=2;i<=right;i++){
-            if (allPrimes[i]){
         boolean[] allPrimes = new boolean[right+1];
         for(int i=2;i<right+1;i++){
-    public boolean[] primeArrBool(int left, int right){
         // int[] numbers = new int[right-left+1];
         boolean[] primes = new boolean[right-left+1];
 class Solution {
+    public boolean[] primeArrBool(int left, int right){
